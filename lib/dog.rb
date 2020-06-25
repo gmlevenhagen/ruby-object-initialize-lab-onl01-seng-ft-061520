3b99e71700125d616eb847f58352dd473a409e17
Garett Levenhagen <garettlevenhagen@gmail.com>
2020-06-25 21:37:22 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,28 +3,6 @@ class Dog
     @name = name
     @breed = name
   end
+end
 
-  def name=(name)
-    @name = name
-  end
 fido=Dog.new("Fido")
-
-  def name
-    @name
-  end
-
-  def breed=(breed)
-    @breed = breed
-  end
-
-  def breed
-    @breed
-  end
-end
-
-
-
-
-
-
-end
